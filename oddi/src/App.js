5ce2b4f829d832ff64490a6b822a72f423dff658
Gabriela Acevedo <gabrieladesiree@gmail.com>
2023-06-29 15:38:44 -0300

    Fixed routes for team component

--- oddi/src/App.js
+++ oddi/src/App.js
@@ -1,33 +1,21 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import AboutOddi from "./components/AboutOddi";
-import InterestForm from "./components/InterestForm";
+import Home from "./components/Home";
 import Footer from "./components/Footer";
-import Landing from "./components/Landing";
 import Copyright from "./components/Copyright";
-import WhyOddi from "./components/WhyOddi";
 import TheTeam from "./components/TheTeam";
 
 function App() {
   return (
     <div>
-
-    <div>
       <Navbar />
-      <Landing />
-      <AboutOddi />
-      <WhyOddi />
-      <InterestForm />
-      <Footer />
-      <Copyright />
-
-      
-    </div>
-
-    <Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/equipo" element={<TheTeam />} />
       </Routes>
+      <Footer />
+      <Copyright />
     </div>
   );
 }
