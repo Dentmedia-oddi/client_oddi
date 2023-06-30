9f873fb05a378e03f1f2c5b523f108e93c5db077
Gabriela Acevedo <gabrieladesiree@gmail.com>
2023-06-30 15:56:56 -0300

    Change model on contact form

--- oddi/src/components/InterestForm.jsx
+++ oddi/src/components/InterestForm.jsx
@@ -1,35 +1,35 @@
 import React from "react";
 
 function InterestForm() {
-  return (
+  return  (
     <div className="w-full py-16 text-white px-4">
       <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
         <div className="lg:col-span-2 my-4">
           <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold pb-4">
-            ¿Te gustaría participar en nuestra <br /> prueba piloto?
+            ¿Tienes dudas respecto a ODDI?
           </h1>
-          <p>Indicanos tu correo y te contactaremos.</p>
+          <p>Escribenos y con gusto aclararemos cualquier inquietud que tengas.</p>
         </div>
         <div className="my-4">
           <div className="flex flex-col sm:flex-row mx-auto items-center justify-between w-full">
-           
             <form>
             <input
-              className="p-3 my-2 w-[300px] flex  rounded-md text-black"
+              className="p-3 my-2 w-[400px] flex  rounded-md text-black"
               type="text"
               placeholder="Ingresa tu nombre y apellido"
             />
-            <input
-              className="p-3 my-2 flex w-full rounded-md text-black"
-              type="text"
-              placeholder="Indica tu especialidad"
-            />
               <input
-                className="p-3 flex w-full rounded-md text-black"
+                className="p-3 my-2 w-[400px] flex  rounded-md text-black"
                 type="email"
                 placeholder="Ingresa tu correo electrónico"
               />
-              <button className="bg-[#1DCAD3] w-[200px] rounded-md font-medium ml-4 my-6 px-6 py-3 text-[#250E62]">
+              <textarea
+              className="p-3 my-2 w-[400px] flex  rounded-md text-black"
+              type="message"
+              rows="10"
+              placeholder="Ingresa tu mensaje"
+              />
+              <button className="bg-[#1DCAD3] w-[100px] rounded-md font-medium ml-auto my-6 px-6 py-3 text-[#250E62]">
                 Enviar
               </button>
             </form>
