63cc15e03782670b706ab9693c762ebba1d04920
Gabriela Acevedo <gabrieladesiree@gmail.com>
2023-07-11 17:02:55 -0300

    added new logos

--- oddi/src/components/Copyright.jsx
+++ oddi/src/components/Copyright.jsx
@@ -5,7 +5,7 @@ function Copyright() {
     <div className="w-full py-2 text-white px-4 text-center">
       <div className="mt-2">
         <span className="text-sm">
-          &copy; 2023 Dent Media. Todos los derechos reservados.
+          &copy; 2023 ODDI. Todos los derechos reservados.
         </span>
       </div>
     </div>
