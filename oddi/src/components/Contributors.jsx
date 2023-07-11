33cabeac0bba2ff7bad506a290a4f9fbed8cbaee
Gabriela Acevedo <gabrieladesiree@gmail.com>
2023-07-11 16:43:39 -0300

    Contributors component changes

--- oddi/src/components/Contributors.jsx
+++ oddi/src/components/Contributors.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import compluemprende from "../assets/logo-compluemprende-2-.jpg";
 
-
 function Contributors() {
   return (
-    <div className="w-full py-12 bg-white px-4 text-[#0076A5]">
-      <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
-        <div className="lg:col-span-2 my-4">
-          <h3 className="md:text-xl sm:text-xl text-xl font-bold pb-4">
-            Colaboradores:
-          </h3>
-          
+    <div name="about" className="w-full bg-white py-12 px-4">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+        <div className="flex flex-col justify-center">
+          <h3 className="text-[#0076A5] font-bold">COLABORADORES</h3>
           <a
             href="https://www.ucm.es/compluemprende"
             target="_blank"
@@ -20,10 +16,9 @@ function Contributors() {
               src={compluemprende}
               alt="complulogo"
               style={{ width: "200px" }}
+              className="py-6"
             />
           </a>
-
-          
         </div>
       </div>
     </div>
