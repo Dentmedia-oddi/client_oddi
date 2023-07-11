a29996f565a5cc2bd763b1285b0394e052100782
Gabriela Acevedo <gabrieladesiree@gmail.com>
2023-07-11 10:41:20 -0300

    contact form configuration with getform

--- oddi/src/components/InterestForm.jsx
+++ oddi/src/components/InterestForm.jsx
@@ -1,33 +1,54 @@
-import React from "react";
+import React, { useRef } from "react";
+
+const getForm = "https://getform.io/f/d06f2036-e491-45dc-87ea-b8fdbdb6f786";
 
 function InterestForm() {
-  return  (
+  const formRef = useRef(null);
+
+  const handleSubmit = () => {
+    setTimeout(() => {
+      formRef.current.reset();
+    }, 500);
+    formRef.current.submit();
+  };
+
+  return (
     <div className="w-full py-16 text-white px-4">
       <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
         <div className="lg:col-span-2 my-4">
           <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold pb-4">
             ¿Quieres saber más?
           </h1>
-          <p>Escribenos y con gusto aclararemos cualquier inquietud que tengas.</p>
+          <p>
+            Escribenos y con gusto aclararemos cualquier inquietud que tengas.
+          </p>
         </div>
         <div className="my-4">
           <div className="flex flex-col sm:flex-row mx-auto items-center justify-between w-full">
-            <form>
-            <input
-              className="p-3 my-2 w-[400px] flex  rounded-md text-black"
-              type="text"
-              placeholder="Ingresa tu nombre y apellido"
-            />
+            <form
+              ref={formRef}
+              method="POST"
+              action={getForm}
+              onSubmit={handleSubmit}
+            >
+              <input
+                className="p-3 my-2 w-[400px] flex  rounded-md text-black"
+                type="text"
+                placeholder="Indicanos tu nombre y apellido"
+                name='name'
+              />
               <input
                 className="p-3 my-2 w-[400px] flex  rounded-md text-black"
                 type="email"
-                placeholder="Ingresa tu correo electrónico"
+                placeholder="Tu correo electrónico"
+                name='email'
               />
               <textarea
-              className="p-3 my-2 w-[400px] flex  rounded-md text-black"
-              type="message"
-              rows="10"
-              placeholder="Ingresa tu mensaje"
+                className="p-3 my-2 w-[400px] flex  rounded-md text-black"
+                type="message"
+                rows="10"
+                placeholder="Ingresa tu mensaje"
+                name='message'
               />
               <button className="bg-[#1DCAD3] w-[100px] rounded-md font-medium ml-auto my-6 px-6 py-3 text-[#250E62]">
                 Enviar
