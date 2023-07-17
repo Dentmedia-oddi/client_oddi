9f1bf18bb27afa9b6a5369c8097a515953fd912b
Gabriela Acevedo <gabrieladesiree@gmail.com>
2023-07-17 19:24:58 -0300

    general adjustments for deploy

--- oddi/src/components/UnderConstruction.jsx
+++ oddi/src/components/UnderConstruction.jsx
@@ -11,20 +11,20 @@ function UnderConstruction() {
         <div className="max-w-[1240px] mx-auto grid md:grid-cols-1">
           <img className="w-[450px] mx-auto" src={under} alt="Picunder" />
           <div className="flex flex-col justify-center">
-            <p className="text-[#0076A5] font-bold text-center">
+            <p className="text-[#0076A5] font-bold text-center md:text-4xl sm:text-2xl text-xl  md:py-2">
               ¡PLANTILLA EN CONSTRUCCIÓN!
             </p>
             <br />
-            <p className="text-center">
-            Pronto podrás disfrutar de las plantillas para especialidades odontológicas. Deja {" "}
+            <p className="text-center md:text-xl sm:text-xl text-xl font-Regular">
+            Pronto podrás disfrutar de las plantillas para especialidades odontológicas. <br/> Deja {" "}
               <a
                 href="/#interest-form"
                 onClick={() => navigate("/#interest-form")}
                 className="text-[#0076A5] font-bold underline"
               >
-                aquí tu email 
+                aquí
               </a>
-              {" "} y accede a la prueba gratuita.
+              {" "}tu email y accede a la prueba gratuita.
             </p>
           </div>
         </div>
