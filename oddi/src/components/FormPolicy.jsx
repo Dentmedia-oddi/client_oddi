f6def741cd29866d931177fd6082ede38d8f0ccf
Gabriela Acevedo <gabrieladesiree@gmail.com>
2023-09-15 17:31:20 -0300

    General changes

---
+++ oddi/src/components/FormPolicy.jsx
@@ -0,0 +1,178 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function FormPolicy() {
+  return (
+    <div name="cookies" id="1" className="w-full bg-white py-9 px-4">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-1">
+        <div className="flex flex-col justify-center text-justify">
+          <br />
+          <p className="text-[#0076A5] font-regular">
+            [ODDI]
+            <br />
+            Política de Protección de Datos Personales y Descargo de
+            Responsabilidad (DISCLAIMER)
+            <br />
+            <br />
+            Fecha de última actualización: [01/08/2023]
+          </p>
+          <br />
+          <br />
+          <p>
+            Introducción
+            <br />
+            En [ODDI], proporcionamos una plantilla de resumen ("la Plantilla")
+            que permite a los usuarios agregar información personal, incluidos
+            nombres, correos electrónicos, especialidades y otros datos
+            relevantes ("Datos Personales") con el único propósito de generar un
+            resumen personalizado. Es importante destacar que esta Plantilla no
+            almacena los Datos Personales y solo los procesa temporalmente para
+            permitir a los usuarios copiar toda esa información en otro sitio.
+            Esta política de protección de datos personales y descargo de
+            responsabilidad complementa nuestra política de privacidad y
+            describe cómo se manejan los Datos Personales en la Plantilla de
+            acuerdo con las leyes y regulaciones de la Unión Europea, en
+            particular el Reglamento General de Protección de Datos (RGPD) y la
+            Ley Orgánica 3/2018, de Protección de Datos Personales y garantía de
+            los derechos digitales (LOPDGDD) en España.
+            <br />
+            <br />
+            Responsable del Tratamiento de Datos Personales
+            <br />
+            [ODDI] actúa como el Responsable del Tratamiento de los Datos
+            Personales procesados en la Plantilla. Nuestra información de
+            contacto es la siguiente:
+            <br />
+            <br />
+            [ODDI]
+            <br />
+            [info@oddi.es]
+            <br />
+            [Madrid, España]
+            <br />
+            <br />
+            Procesamiento de Datos Personales
+            <br />
+            La Plantilla permite a los usuarios agregar Datos Personales de
+            manera voluntaria para generar un resumen personalizado. Es
+            importante destacar que no almacenamos los Datos Personales en la
+            Plantilla. Solo se procesan temporalmente para permitir a los
+            usuarios copiar toda esa información y pegarla en otro sitio de su
+            elección.
+            <br />
+            <br />
+            Propósito del Tratamiento de Datos Personales
+            <br />
+            El único propósito del procesamiento de Datos Personales en la
+            Plantilla es generar un resumen personalizado, que luego se puede
+            copiar y pegar en otro sitio de elección del usuario. No utilizamos
+            los Datos Personales para ningún otro fin, ni los compartimos con
+            terceros.
+            <br />
+            <br />
+            Responsabilidad del Especialista
+            <br />
+            En el caso de que un usuario de la Plantilla sea un especialista que
+            recopila Datos Personales de sus pacientes, es importante destacar
+            que dicho especialista es el único responsable de la gestión y
+            tratamiento de los Datos Personales de sus pacientes de acuerdo con
+            las leyes y regulaciones aplicables. La Plantilla solo se
+            proporciona como una herramienta de resumen, y la responsabilidad de
+            cumplir con las obligaciones legales y éticas relacionadas con la
+            protección de datos de los pacientes recae totalmente en el
+            especialista.
+            <br />
+            <br />
+            Base Legal para el Tratamiento de Datos Personales
+            <br />
+            El procesamiento de Datos Personales en la Plantilla se basa en el
+            consentimiento expreso que los usuarios otorgan al agregar
+            voluntariamente sus Datos Personales en la Plantilla con el único
+            propósito de generar un resumen personalizado.
+            <br />
+            <br />
+            Seguridad de los Datos Personales
+            <br />
+            Nos tomamos en serio la seguridad de los Datos Personales y hemos
+            implementado medidas técnicas y organizativas para protegerlos
+            contra el acceso no autorizado, la divulgación, la alteración o la
+            destrucción accidental o ilícita. Sin embargo, es importante tener
+            en cuenta que, debido a la naturaleza temporal del procesamiento, no
+            podemos garantizar la seguridad total de los Datos Personales una
+            vez que el usuario los copia y pega en otro sitio.
+            <br />
+            <br />
+            Descargo de Responsabilidad
+            <br />
+            Los usuarios reconocen por la presente que las herramientas en línea
+            listadas en www.oddi.es no están certificadas como "dispositivos
+            médicos" y que los usuarios solo tienen derecho a acceder al "estado
+            actual" tal como se publica en línea. Los usuarios utilizan estas
+            herramientas en línea bajo su propia responsabilidad y riesgo.
+            Ninguna de estas herramientas guarda los datos ingresados en sus
+            formularios en línea.
+            <br />
+            <br />
+            Los autores de www.oddi.es no ofrecen garantías sobre el uso de sus
+            herramientas para un propósito específico. Los autores no se hacen
+            responsables de los daños resultantes del uso de estas herramientas
+            en línea de forma privada o en la práctica médica.
+            <br />
+            <br />
+            Los autores agradecen cualquier comentario sobre sus herramientas en
+            línea. Sin embargo, los autores no están obligados a proporcionar
+            ningún tipo de soporte técnico, médico u otro tipo de soporte.
+            <br />
+            <br />
+            Aceptación de la Política y Descargo de Responsabilidad
+            <br />
+            Al utilizar nuestro sitio web y sus herramientas, aceptas esta
+            política de protección de datos personales y descargo de
+            responsabilidad en su totalidad. Si no estás de acuerdo con esta
+            política, por favor no utilices nuestro sitio web ni las
+            herramientas en línea. Además, te pedimos que indiques si eres un
+            profesional de la salud marcando la casilla correspondiente.
+            <br />
+            <br />
+            Cambios en esta Política
+            <br />
+            Nos reservamos el derecho de modificar esta política de protección
+            de datos personales y descargo de responsabilidad en cualquier
+            momento. Cualquier cambio será publicado en esta página, y la fecha
+            de la última actualización se revisará al principio del documento.
+            <br />
+            <br />
+            Contacto
+            <br />
+            Si tienes alguna pregunta o inquietud acerca de nuestra política de
+            protección de datos personales y descargo de responsabilidad o sobre
+            cómo manejamos la información personal en la Plantilla, por favor
+            contáctanos a través de{" "}
+            <a href="mailto:info@oddi.es" className="text-[#0076A5]">
+              [info@oddi.es]
+            </a>
+            .
+            <br />
+            <br />
+            Gracias por leer nuestra política de protección de datos personales
+            y descargo de responsabilidad (Disclaimer).
+          </p>
+          <br />
+          <p className="text-[#0076A5] font-bold italic text-right">
+            ODDI - Simplifying records
+          </p>
+          <br />
+        </div>
+      </div>
+
+      <br />
+      <Link to="/">
+        <button className="bg-white w-[100px] rounded-md font-medium ml-auto my-4 px-6 text-[#250E62]  group border-2 flex items-center hover:bg-[#1DCAD3] hover:border-[#1DCAD3]">
+          Volver
+        </button>
+      </Link>
+    </div>
+  );
+}
+
+export default FormPolicy;
--- oddi/src/components/UnderConstruction.jsx
+++ oddi/src/components/UnderConstruction.jsx
@@ -1,40 +1,41 @@
 import React from "react";
 import under from "../assets/Underconstruction.png";
-import { useNavigate } from "react-router-dom";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function UnderConstruction() {
-  const navigate = useNavigate();
-
   return (
     <div>
       <div name="construction" className="w-full bg-white py-20 px-4">
         <div className="max-w-[1240px] mx-auto grid md:grid-cols-1">
-          <img className="w-[450px] mx-auto" src={under} alt="Imagen de sitio en construccion" title="construccion" />
+          <img
+            className="w-[450px] mx-auto"
+            src={under}
+            alt="Imagen de sitio en construccion"
+            title="construccion"
+          />
           <div className="flex flex-col justify-center">
             <p className="text-[#0076A5] font-bold text-center md:text-4xl sm:text-2xl text-xl  md:py-2">
               ¡PLANTILLA EN CONSTRUCCIÓN!
             </p>
             <br />
             <p className="text-center md:text-xl sm:text-xl text-xl font-Regular">
-            Pronto podrás disfrutar de las plantillas para especialidades odontológicas. <br/> Deja {" "}
+              Pronto podrás disfrutar de las plantillas para especialidades
+              odontológicas. <br /> Escribenos a{" "}
               <a
-                href="/#interest-form"
-                onClick={() => navigate("/#interest-form")}
-                className="text-[#0076A5] font-bold underline"
+                href="mailto:info@oddi.es"
+                className="text-[#0076A5] font-bold"
               >
-                aquí
-              </a>
-              {" "}tu email y accede a la prueba gratuita.
+                info@oddi.es
+              </a>{" "}
+              y accede a la prueba gratuita.
             </p>
           </div>
-          
         </div>
         <Link to="/" className="flex justify-end items-center pt-10 px-2">
-        <button className="bg-white w-[100px] rounded-md font-medium ml-auto my-4 px-6 text-[#250E62]  group border-2 flex items-center hover:bg-[#1DCAD3] hover:border-[#1DCAD3]">
-          Volver
-        </button>
-      </Link>
+          <button className="bg-white w-[100px] rounded-md font-medium ml-auto my-4 px-6 text-[#250E62]  group border-2 flex items-center hover:bg-[#1DCAD3] hover:border-[#1DCAD3]">
+            Volver
+          </button>
+        </Link>
       </div>
     </div>
   );
